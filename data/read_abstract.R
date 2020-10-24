b9eb05209cd9287d5247c247311928e3b1427440
yifanl4 <70758024+yifanl4@users.noreply.github.com>
2020-10-24 18:13:07 -0500

    Add files via upload

---
+++ data/read_abstract.R
@@ -0,0 +1,40 @@
+library(pubmed.mineR)
+
+#(search key words: covid 19)
+a1 = readabsnew("abstract-covid19-set.txt")
+
+#(search key words: covid 19 characteristics)
+a2 = readabsnew("abstract-covid19cha-set.txt")
+
+#(search key words: covid 19 control)
+a3 = readabsnew("abstract-covid19con-set.txt")
+
+#(search key words: covid 19 quarantine)
+a4 = readabsnew("abstract-covid19qua-set.txt")
+
+#(search key words: covid 19 effects)
+a5 = readabsnew("abstract-covid19eff-set.txt")
+
+#(search key words: against covid 19)
+a6 = readabsnew("abstract-againstcov-set.txt")
+
+#(search key words: covid 19 drug)
+a7 = readabsnew("abstract-covid19dru-set.txt")
+
+#(search key words: covid 19 china)
+a8 = readabsnew("abstract-covid19chi-set.txt")
+
+#(search key words: covid 19 the united states)
+a9 = readabsnew("abstract-covid19the-set.txt")
+
+#(search key words: covid 19 vaccine)
+a10 = readabsnew("abstract-covid19vac-set.txt")
+
+#(search key words: covid 19 polices)
+a11 = readabsnew("abstract-covid19pol-set.txt")
+
+#(search key words: covid 19 age)
+a12 = readabsnew("abstract-covid19age-set.txt")
+
+#(search key words: covid 19 covid 19 herd immune)
+a13 = readabsnew("abstract-covid19her-set.txt")
